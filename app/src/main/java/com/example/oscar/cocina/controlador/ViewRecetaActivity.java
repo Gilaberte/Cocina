4117f253cc5f8e67e5cd056edd88adf4ab4629e4
manana <manana@baroja21.local>
2015-10-08 13:10:34 +0200

    Prueba Git

--- app/src/main/java/com/example/oscar/cocina/controlador/ViewRecetaActivity.java
+++ app/src/main/java/com/example/oscar/cocina/controlador/ViewRecetaActivity.java
@@ -91,6 +91,7 @@ public class ViewRecetaActivity extends Activity {
     }
 
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
